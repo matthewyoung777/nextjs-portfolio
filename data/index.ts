de84b578a9e116adf6555cad4ad61756d9886fc3
matthewyoung777 <y.matt12@gmail.com>
2025-01-29 00:43:42 -0800

    update forge fitness image

--- data/index.ts
+++ data/index.ts
@@ -73,11 +73,11 @@ export const gridItems = [
 export const projects = [
     {
         id: 1,
-        title: "3D Solar System Planets to Explore",
-        des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-        img: "/p1.svg",
+        title: "Forge Fitness",
+        des: "Gamifyng fitness to make it more fun and engaging for users to chase their fitness goals.",
+        img: "/forge-fitness.svg",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        link: "/ui.earth.com",
+        link: "https://mailchi.mp/6190a8efe0c0/forge-fitness-signup-form",
     },
     {
         id: 2,
