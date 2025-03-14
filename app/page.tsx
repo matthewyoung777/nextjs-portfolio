8202464d981a60d25c817566076c860683200abd
matthewyoung777 <y.matt12@gmail.com>
2025-03-14 15:18:47 -0700

    fix moving borders

--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,36 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Clients from "@/components/Clients";
-import Chatbox from "@/components/ChatBox";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
-import { TimelineList } from "@/components/TimelineList";
 
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
     return (
-        <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
-            <div className="max-w-7xl w-full">
-                <FloatingNav navItems={navItems} />
-                <Hero />
-                <Grid />
-                <RecentProjects />
-                <Clients />
-                <Experience />
+        <>
+            <head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+                />
+            </head>
+            <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+                <div className="max-w-7xl w-full">
+                    <FloatingNav navItems={navItems} />
+                    <Hero />
+                    <Grid />
+                    <RecentProjects />
+                    <Clients />
+                    <Experience />
 
-                {/* <TimelineList /> */}
-                {/* <Approach /> */}
-                <Footer />
-            </div>
-        </main>
+                    {/* <TimelineList /> */}
+                    {/* <Approach /> */}
+                    <Footer />
+                </div>
+            </main>
+        </>
     );
 };
 
