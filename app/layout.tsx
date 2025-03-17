79b907414f781fc06c2f18badc47bb268a8880e7
matthewyoung777 <y.matt12@gmail.com>
2025-03-17 00:34:03 -0700

    website manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,24 +19,20 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
-                <link
-                    rel="icon"
-                    href="/favicon_io/apple-touch-icon.png"
-                    sizes="any"
-                />
+                <link rel="icon" href="/apple-touch-icon.png" sizes="any" />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="32x32"
-                    href="/favicon_io/favicon-32x32.png"
+                    href="/favicon-32x32.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="16x16"
-                    href="/favicon_io/favicon-16x16.png"
+                    href="/favicon-16x16.png"
                 />
-                <link rel="manifest" href="/favicon_io/site.webmanifest" />
+                <link rel="manifest" href="/site.webmanifest" />
             </head>
             <body className={inter.className}>
                 <ThemeProvider
