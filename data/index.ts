f436f6af09fd9c1a321373a97b24f0e192ff2584
matthewyoung777 <y.matt12@gmail.com>
2025-03-20 20:53:41 -0700

    decrease size of map box in bento grid

--- data/index.ts
+++ data/index.ts
@@ -21,7 +21,7 @@ export const gridItems = [
         id: 2,
         title: "I'm very flexible with time zone communications",
         description: "",
-        className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+        className: "lg:col-span-2 md:col-span-3 md:row-span-1",
         imgClassName: "",
         titleClassName: "justify-start",
         img: "",
